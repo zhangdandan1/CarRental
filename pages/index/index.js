5565b1be0b195c15e4d09816dd1e1d069912581d
zdd <17665126221@163.com>
2017-12-25 22:30:27 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        currentTab: 0
+    },
+    onLoad: function(options) {
+        // 页面初始化 options为页面跳转所带来的参数
+
+    },
+    //滑动切换
+    swiperTab: function(e) {
+        var that = this;
+        that.setData({
+            currentTba: e.detail.current
+        });
+    },
+    //点击切换
+    clickTab: function(e) {
+
+        var that = this;
+
+        if (this.data.currentTab === e.target.dataset.current) {
+            return false;
+        } else {
+            that.setData({
+                currentTab: e.target.dataset.current
+            })
+        }
+    }
+
+})
