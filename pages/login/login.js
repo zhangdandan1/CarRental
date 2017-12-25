caf4da99464a6fa2691b340104e6bebd736d0f4f
ohraymaster <ohraymaster@163.com>
2017-12-26 01:39:10 +0800

    login button

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,11 @@
 Page({
-
+  data: {
+    loginMode: "center"
+  },  
+  login: function(e) {
+    var that = this;
+    this.setData({loginMode: "scaleToFill"});
+    setTimeout(function () { that.setData({ loginMode: "center" });}, 700); 
+    console.log("need to be implemented");
+  },
 })
